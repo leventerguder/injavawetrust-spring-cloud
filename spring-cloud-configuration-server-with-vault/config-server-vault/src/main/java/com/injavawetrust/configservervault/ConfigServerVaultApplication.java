075092c0314b372577377a09561ab31c5aca8814
Levent Erguder <erguder.levent@gmail.com>
2021-10-23 12:14:15 +0300

    pom.xml update

--- spring-cloud-configuration-server-with-vault/config-server-vault/src/main/java/com/injavawetrust/configservervault/ConfigServerVaultApplication.java
+++ spring-cloud-configuration-server-with-vault/config-server-vault/src/main/java/com/injavawetrust/configservervault/ConfigServerVaultApplication.java
@@ -12,4 +12,5 @@ public class ConfigServerVaultApplication {
         SpringApplication.run(ConfigServerVaultApplication.class, args);
     }
 
+    // curl -X "GET" "http://localhost:8701/config-client/dev" -H "X-Config-Token: your vault token"
 }
