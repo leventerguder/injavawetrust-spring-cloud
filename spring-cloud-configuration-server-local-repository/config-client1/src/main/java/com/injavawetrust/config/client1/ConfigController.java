997d4c553b11506b7425eaec0f14b422ecf70ec2
Levent Erguder <erguder.levent@gmail.com>
2021-10-23 10:58:05 +0300

    pom.xml update

--- spring-cloud-configuration-server-local-repository/config-client1/src/main/java/com/injavawetrust/config/client1/ConfigController.java
+++ spring-cloud-configuration-server-local-repository/config-client1/src/main/java/com/injavawetrust/config/client1/ConfigController.java
@@ -1,23 +1,21 @@
 package com.injavawetrust.config.client1;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/configs")
 public class ConfigController {
 
-    @Autowired
-    private PropertiesDto propertiesDto;
+    private final PropertiesDto propertiesDto;
+
+    public ConfigController(PropertiesDto propertiesDto) {
+        this.propertiesDto = propertiesDto;
+    }
 
     @GetMapping()
     public PropertiesDto getPropertiesDto() {
         return propertiesDto;
     }
-
 }
